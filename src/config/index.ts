60a38747ef32fd4de8e534017b4c3113d26c5437
basak.kocakaya <basak.kocakaya12@gmail.com>
2025-01-07 23:44:56 +0300

    fix config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-const API_URL = '/https://supplyin-backend.onrender.com';
+const API_URL = 'https://supplyin-backend.onrender.com';
 //http://localhost:5050
 //https://supplyin-backend.onrender.com
 
